60e0c40a3f10182a83295f16f7322dc62d7268f4
Illuzzion <79fz270704@gmail.com>
2017-04-17 16:44:19 +0300

    first commit

---
+++ blog/home/models.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import, unicode_literals
+
+from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailcore.fields import RichTextField
+from wagtail.wagtailcore.models import Page
+
+
+class HomePage(Page):
+    # pass
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body', classname="full")
+    ]
