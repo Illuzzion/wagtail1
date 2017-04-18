8541657e65dbaf3ea681979469518174bb313fdc
Illuzzion <79fz270704@gmail.com>
2017-04-18 10:53:54 +0300

    add gallery, tags to blog_page

--- blog/newblog/models.py
+++ blog/newblog/models.py
@@ -1,22 +1,46 @@
 from django.db import models
-from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from modelcluster.fields import ParentalKey
+from taggit.models import TaggedItemBase
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel
 from wagtail.wagtailcore.fields import RichTextField
-from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.models import Page, Orderable
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 from wagtail.wagtailsearch import index
 
 
 class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
 
+    def get_context(self, request, *args, **kwargs):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super(BlogIndexPage, self).get_context(request)
+        # select only published posts
+        blogpages = self.get_children().live().order_by('-first_published_at')
+        context['blogpages'] = blogpages
+        return context
+
     content_panels = Page.content_panels + [
         FieldPanel('intro', classname="full")
     ]
 
 
+class BlogPageTag(TaggedItemBase):
+    content_object = ParentalKey('BlogPage', related_name='Тэги')
+
+
 class BlogPage(Page):
     date = models.DateField("Post date")
-    intro = models.CharField(max_length=250)
-    body = RichTextField(blank=True)
+    intro = models.CharField('Интро', max_length=250)
+    body = RichTextField('Контент', blank=True)
+    tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
+
+    def main_image(self):
+        gallery_item = self.gallery_images.first()
+        if gallery_item:
+            return gallery_item.image
+        else:
+            return None
 
     search_fields = Page.search_fields + [
         index.SearchField('intro'),
@@ -24,7 +48,24 @@ class BlogPage(Page):
     ]
 
     content_panels = Page.content_panels + [
-        FieldPanel('date'),
+        MultiFieldPanel([
+            FieldPanel('date'),
+            FieldPanel('tags'),
+        ], heading='Информация'),
         FieldPanel('intro'),
         FieldPanel('body', classname="full"),
+        InlinePanel('gallery_images', label="Галлерея"),
+    ]
+
+
+class BlogPageGalleryImage(Orderable):
+    page = ParentalKey(BlogPage, related_name='gallery_images')
+    image = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
+    )
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = [
+        ImageChooserPanel('image'),
+        FieldPanel('caption'),
     ]
